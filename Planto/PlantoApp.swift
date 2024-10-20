49e10f7f9474bb56b7a0b2fa59314924e3dc1708
AbeerX <Abeeralali021@gmail.com>
2024-10-20 09:30:53 +0300

    Initial Commit

---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Abeer on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
